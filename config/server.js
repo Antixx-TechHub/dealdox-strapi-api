753ea1b463b49de4baae7737ea268f21d13dc080
AntixxTechHub <info@antixxtechhub.com>
2023-06-13 18:10:13 +0530

    api added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: 'https://cms.dealdox.io/',
+  url: 'https://dealdoxstrapi,pbwebvision.com/',
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
