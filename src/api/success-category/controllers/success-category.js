48b4873ce176c4e2250a95fbedcdaf6021f07dbf
AntixxTechHub <info@antixxtechhub.com>
2023-06-19 17:05:29 +0530

    Ctegory API added

---
+++ src/api/success-category/controllers/success-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * success-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::success-category.success-category');
---
+++ src/api/success-category/services/success-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * success-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::success-category.success-category');
